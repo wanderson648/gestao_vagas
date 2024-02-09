9b3e009f3c542ca80c467d6c5d7f662d30611e3e
wanderson648 <testepedroca@gmail.com>
2024-02-08 22:24:50 -0300

    implementa validacao

--- src/main/java/com/wo/gestao_vagas/modules/candidates/controllers/CandidateController.java
+++ src/main/java/com/wo/gestao_vagas/modules/candidates/controllers/CandidateController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.wo.gestao_vagas.modules.candidates.CandidateEntity;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
     }
